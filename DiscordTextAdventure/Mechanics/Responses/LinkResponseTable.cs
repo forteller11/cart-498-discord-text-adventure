7b6059aa30f5012088c623d9da5d6f24ff3919ab
forteller11 <charly.yan.miller@gmail.com>
2021-01-27 01:19:16 -0500

    debugged link reponses
    had some weird closure inconsistencies which arose from where I was instantiating my arguments (inside versus outside the lambda)...

--- DiscordTextAdventure/Mechanics/Responses/LinkResponseTable.cs
+++ DiscordTextAdventure/Mechanics/Responses/LinkResponseTable.cs
@@ -13,26 +13,70 @@ namespace DiscordTextAdventure.Mechanics.Responses
         public static readonly LinkResponse Animals;
         public static readonly LinkResponse [] LinkResponses;
 
-        private static readonly string[] DnDTypeWords = new[]
+        private static RandomPhrasePicker picker = new RandomPhrasePicker(
+            "A rogue breaks away from their argument over what constitues a skill check, seemingly intrigued by the *content* you posted.",
+            "A Dwarf stops admiring their gold bars momentarily as your *content* is noticed.");
+        private static readonly string[] DnDRelatedWords = {
+            "tolken", "orc", "orcs", "elves", "elf", "elvish", "gnome", "dnd", "dungeon", "dungeons", "dragons", 
+                "lord", "ring", "sword", "swords", "axe", "battle", "war", "skill", "dice", "d20", "skillcheck", "gnome", 
+                "gnomes", "bar", "drink", "quest", "quests", "gold", "tolken", "fantasy", "druid", "magic", "spells", "table", 
+                "top", "tabletop", "ttrp", "role", "playing", "game", "games"};
+        
+        private static readonly string[] PokemonRelatedWords = new[]
             {"tolken", "orc", "orcs", "elves", "elf", "elvish", "gnome", "dnd", "dungeon", "dungeons", "dragons", "lord", "ring", "sword", "swords"};
 
         static LinkResponseTable()
         {
-            DnD = new LinkResponse(OnDnDLink, null);
-            Pokemon = new LinkResponse(PokemonLink, null);
-            Animals = new LinkResponse(AnimalsLink, null);
             
-            
-            
-            void OnDnDLink (LinkResponseEventArgs e)
+            //have to instantiate outside lamdba for some reason or else behavior breaks
             {
-                if (e.PostedRoom.MessageChannel.Id == e.Session.RoomManager.DnD.MessageChannel.Id
+                var relevantPhrasePicker = new RandomPhrasePicker(
+                    "A rogue breaks away from their argument over what constitutes a skill check, seemingly intrigued by the *content* you posted.",
+                    "A Dwarf stops admiring their gold bars momentarily as your *content* is noticed.",
+                    "You hear laughter come from around the tables");
+                
+                var irrelevantPhrasePicker = new RandomPhrasePicker(
+                    "The members' seem to be more interested in their D20's...",
+                    "This didn't seem to scratch anyone's high-fantasy flavoured itch",
+                    "The member's stayed focused on their main quests, unmoved by the distraction.");
+                
+                DnD = new LinkResponse(e =>
+                        InterestingLinkCheck(e, DnDRelatedWords, e.Session.RoomManager.DnD,
+                            relevantPhrasePicker,
+                            irrelevantPhrasePicker),
+                    null);
+            }
+            
+            {
+                var relevantPhrasePicker = new RandomPhrasePicker(
+                    "A rogue breaks away from their argument over what constitutes a skill check, seemingly intrigued by the *content* you posted.",
+                    "A Dwarf stops admiring their gold bars momentarily as your *content* is noticed.",
+                    "You hear laughter come from around the tables");
+                
+                var irrelevantPhrasePicker = new RandomPhrasePicker(
+                    "The members' seem to be more interested in their D20's...",
+                    "This didn't seem to scratch anyone's high-fantasy flavoured itch",
+                    "The member's stayed focused on their main quests, unmoved by the distraction.");
+                
+                Pokemon = new LinkResponse(e =>
+                        InterestingLinkCheck(e, PokemonTypeWords, e.Session.RoomManager.Pokemon,
+                            relevantPhrasePicker,
+                            irrelevantPhrasePicker),
+                    null);
+            }
+            //Animals = new LinkResponse(AnimalsLink, null);
+            
+            
+            void InterestingLinkCheck (LinkResponseEventArgs e, string[] relevantWords, Room relevantRoom, 
+                RandomPhrasePicker phraseOnRelevant, RandomPhrasePicker phraseOnNotRelevant)
+            {
+                if (e.PostedRoom.MessageChannel.Id == relevantRoom.MessageChannel.Id
                     && e.Link.IsValid)
                 {
-                    if (ContainsWords(e.Link.Words, DnDTypeWords, 1))
-                    {
-                        e.PostedRoom.MessageChannel.SendMessageAsync("The dnd members seem to like that");
-                    }
+                    if (ContainsWords(e.Link.Words, relevantWords, 1))
+                        e.PostedRoom.MessageChannel.SendMessageAsync(phraseOnRelevant.GetNextPhrase());
+                    else
+                        e.PostedRoom.MessageChannel.SendMessageAsync(phraseOnNotRelevant.GetNextPhrase());
                 }
             }
             
@@ -41,7 +85,7 @@ namespace DiscordTextAdventure.Mechanics.Responses
                 if (e.PostedRoom.MessageChannel.Id == e.Session.RoomManager.DnD.MessageChannel.Id
                     && e.Link.IsValid)
                 {
-                    if (ContainsWords(e.Link.Words, DnDTypeWords, 1))
+                    if (ContainsWords(e.Link.Words, DnDRelatedWords, 1))
                     {
                         e.PostedRoom.MessageChannel.SendMessageAsync("The pokemon members seem to like that");
                     }
@@ -53,7 +97,7 @@ namespace DiscordTextAdventure.Mechanics.Responses
                 if (e.PostedRoom.MessageChannel.Id == e.Session.RoomManager.DnD.MessageChannel.Id
                     && e.Link.IsValid)
                 {
-                    if (ContainsWords(e.Link.Words, DnDTypeWords, 1))
+                    if (ContainsWords(e.Link.Words, DnDRelatedWords, 1))
                     {
                         e.PostedRoom.MessageChannel.SendMessageAsync("The animal lovers seem to like that");
                     }
---
+++ DiscordTextAdventure/Mechanics/Responses/RandomPhrasePicker.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+#nullable enable
+
+namespace DiscordTextAdventure.Mechanics.Responses
+{
+    public class RandomPhrasePicker
+    {
+        private int _counter;
+        private readonly string[] _phrases;
+        private readonly object _lock = new object();
+
+        public RandomPhrasePicker(params string[] phrases)
+        {
+            _phrases = phrases;
+        }
+
+        public string GetNextPhrase()
+        {
+            _counter++;
+
+            int index = _counter % _phrases.Length;
+            Program.DebugLog(index);
+            return _phrases[index];
+        }
+
+      
+    }
+}
